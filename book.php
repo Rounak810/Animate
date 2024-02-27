c41d7e09700350fdd8070dc36af17cc0b59aa87a
Rounak Ghosh <117658194+Rounak810@users.noreply.github.com>
2024-02-27 20:58:39 +0530

    Add files via upload

---
+++ book.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	 <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Book</title>
+  <meta content="" name="description">
+
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <!-- <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+ -->
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <style type="text/css">
+  	
+  	body{
+  		font-family: "Nunito", sans-serif; 
+
+  	}
+  </style>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+</head>
+<body>
+<header id="header" class="header fixed-top">
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+
+      <a href="index.html" class="logo d-flex align-items-center">
+        <img src="assets/img/logo.png" alt="">
+        <span>Animatemate</span>
+      </a>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">About</a></li>
+          <li><a class="nav-link scrollto" href="#services">Services</a></li>
+          <li><a class="nav-link scrollto" href="#pricing">Pricing</a></li>
+        <!--  <li><a class="nav-link scrollto" href="#portfolio">Portfolio</a></li>
+          <li><a class="nav-link scrollto" href="#team">Team</a></li>
+          <li><a href="blog.html">Blog</a></li>
+          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">Drop Down 1</a></li>
+              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Drop Down 2</a></li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li>
+            </ul>
+          </li>
+		  -->
+          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+          <li><a class="getstarted scrollto" href="#about">Get Started</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+<div class="row" style="margin-top: 160px;">
+	<h2 style="text-align: center; margin-bottom: 40px;"><strong>Drone Rental</strong></h2>
+	<div class="container" style="width: 80%;">
+  <div class="row">
+  <div class="col-sm-3">
+    <div class="card">
+    	<div class="card-header">
+    Featured
+  </div>
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+       
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-3">
+    <div class="card">
+    	<div class="card-header">
+    Featured
+  </div>
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-3">
+    <div class="card">
+    	<div class="card-header">
+    Featured
+  </div>
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+       
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-3">
+    <div class="card">
+    	<div class="card-header">
+    Featured
+  </div>
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+
+
+<div class="container" style="margin-top: 50px; width: 50%;">
+  <h2>Book Now</h2>
+  <form action="book.php">
+    <div class="form-group">
+      <label for="email">Name</label>
+      <input type="text" class="form-control" id="email" placeholder="Enter Name" name="email">
+    </div>
+    <br>
+    <div class="form-group">
+      <label for="email">Contact Number</label>
+      <input type="text" class="form-control" id="email" placeholder="Enter Contact No." name="email">
+    </div>
+    <br>
+    <div class="form-group">
+      <label for="pwd">Drone</label>
+      <input type="text" class="form-control" id="pwd" placeholder="Drone Model" name="pwd">
+    </div>
+    <br>
+    <button type="submit" class="btn btn-primary" style="text-align: center;">Book</button>
+	</form>
+
+</div>
+
+
+
+
+  <!-- <div class="row" style="margin-top: 160px;">
+    <div class="col-sm-6" style="">
+    	
+<div class="container" style="padding-top: 10px; padding-bottom: 10px;">
+  <h2 style="color: #012970;">Price</h2>
+              
+  <table class="table table-hover">
+    <thead>
+      <tr>
+        <th>Drone Name</th>
+        <th>Price</th>
+        
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>John</td>
+        <td>Doe</td>
+        
+      </tr>
+      <tr>
+        <td>Mary</td>
+        <td>Moe</td>
+       
+      </tr>
+      <tr>
+        <td>July</td>
+        <td>Dooley</td>
+        
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+
+
+
+    </div>
+
+    <div class="col-sm-6" style="padding-top: 10px; padding-bottom: 10px;">
+    	
+
+    	<div class="container">
+  <h2>Book Now</h2>
+  <form action="">
+    <div class="form-group">
+      <label for="email">Name</label>
+      <input type="text" class="form-control" id="email" placeholder="Enter Name" name="email">
+    </div>
+    <br>
+    <div class="form-group">
+      <label for="email">Contact Number</label>
+      <input type="text" class="form-control" id="email" placeholder="Enter Contact No." name="email">
+    </div>
+    <br>
+    <div class="form-group">
+      <label for="pwd">Drone</label>
+      <input type="text" class="form-control" id="pwd" placeholder="Drone Model" name="pwd">
+    </div>
+    <br>
+    <button type="submit" class="btn btn-primary">Book</button>
+
+      </form>
+
+</div>
+
+
+
+
+    </div>
+  </div>
+   -->
+
+</body>
+</html>
